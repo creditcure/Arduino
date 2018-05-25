d7ec6ed8b41efaa174a63c6d877912b6714fd21d
kmert10 <mertkarakas1997@gmail.com>
2018-05-25 02:01:45 +0000

    Added font to LCD text display. Sample code to write credit card information on screen.

---
+++ LCDScreenTest.py
@@ -0,0 +1,69 @@
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+
+
+# Raspberry Pi configuration.
+DC = 18
+RST = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# Create TFT LCD display class.
+disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+
+# Initialize display.
+disp.begin()
+
+# Clear the display to a red background.
+# Can pass any tuple of red, green, blue values (from 0 to 255 each).
+disp.clear((102, 153, 204))
+
+# Alternatively can clear to a black screen by calling:
+# disp.clear()
+
+# Get a PIL Draw object to start drawing on the display buffer.
+draw = disp.draw()
+
+# Load default font.
+font = ImageFont.load_default()
+
+# Alternatively load a TTF font.
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype('/home/pi/Desktop/verdana.ttf', 20)
+
+# Define a function to create rotated text.  Unfortunately PIL doesn't have good
+# native support for rotated fonts, but this function can be used to make a
+# text image and rotate it so it's easy to paste in the buffer.
+def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
+    # Get rendered font width and height.
+    draw = ImageDraw.Draw(image)
+    width, height = draw.textsize(text, font=font)
+    # Create a new image with transparent background to store the text.
+    textimage = Image.new('RGBA', (width, height), (0,0,0,0))
+    # Render the text.
+    textdraw = ImageDraw.Draw(textimage)
+    textdraw.text((0,0), text, font=font, fill=fill)
+    # Rotate the text image.
+    rotated = textimage.rotate(angle, expand=1)
+    # Paste the text into the image, using it as a mask for transparency.
+    image.paste(rotated, position, rotated)
+
+# Write two lines of white text on the buffer, rotated 90 degrees counter clockwise. 240x320 LCD
+
+# Virtual Credit Card number
+draw_rotated_text(disp.buffer, '1234-1234-1234-1234', (35, 35), 90, font, fill=(255,255,255))
+# Expiration Date
+draw_rotated_text(disp.buffer, '01/24', (70, 180), 90, font, fill=(255,255,255))
+# Special Code
+draw_rotated_text(disp.buffer, '123', (70, 80), 90, font, fill=(255,255,255))
+
+# Write buffer to display hardware, must be called to make things visible on the
+# display!
+disp.display()
+
+
