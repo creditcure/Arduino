cca07ee16aa91f5fc604516bff2c70aadb85df6f
kmert10 <mertkarakas1997@gmail.com>
2018-05-25 05:30:23 +0000

    Fixed pushbutton issues to count expenses. Need to find a pin that works for the ones digit.

--- LCDScreenTest.py
+++ LCDScreenTest.py
@@ -6,7 +6,10 @@ import Adafruit_ILI9341 as TFT
 import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import time
+import RPi.GPIO as gpio
 
+pushButton3, pushButton5, pushButton18 = 3, 5, 40
+sendValue = 0
 # Raspberry Pi configuration.
 DC = 18
 RST = 23
@@ -15,7 +18,10 @@ SPI_DEVICE = 0
 
 # Create TFT LCD display class.
 disp = TFT.ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
-
+# Setup board and pins
+gpio.setup(pushButton18, gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(pushButton3, gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(pushButton5, gpio.IN, pull_up_down=gpio.PUD_UP)
 # Initialize display.
 disp.begin()
 
@@ -34,7 +40,7 @@ font = ImageFont.load_default()
 
 # Alternatively load a TTF font.
 # Some other nice fonts to try: http://www.dafont.com/bitmap.php
-font = ImageFont.truetype('/home/pi/Desktop/verdana.ttf', 20)
+font = ImageFont.truetype('verdana.ttf', 20)
 
 # Define a function to create rotated text.  Unfortunately PIL doesn't have good
 # native support for rotated fonts, but this function can be used to make a
@@ -55,31 +61,50 @@ def draw_rotated_text(image, text, position, angle, font, fill=(255,255,255)):
 
 # Write two lines of white text on the buffer, rotated 90 degrees counter clockwise. 240x320 LCD
 
-# Virtual Credit Card number
+# Real Credit Card number
 draw_rotated_text(disp.buffer, '1234-1234-1234-1234', (35, 35), 90, font, fill=(255,255,255))
 # Expiration Date
 draw_rotated_text(disp.buffer, '01/24', (70, 180), 90, font, fill=(255,255,255))
 # Special Code
 draw_rotated_text(disp.buffer, '123', (70, 80), 90, font, fill=(255,255,255))
-sendValue = 80
 draw_rotated_text(disp.buffer, 'Expense: ' + str(sendValue), (110, 90), 90, font, fill=(255,255,255))
 disp.display()
 # Write buffer to display hardware, must be called to make things visible on the
 # display!
 
-# refresh image
-time.sleep(2)
-disp.clear((102, 153, 204))
-# Virtual Credit Card number
-draw_rotated_text(disp.buffer, '1234-1234-1234-1234', (35, 35), 90, font, fill=(255,255,255))
-# Expiration Date
-draw_rotated_text(disp.buffer, '01/24', (70, 180), 90, font, fill=(255,255,255))
-# Special Code
-draw_rotated_text(disp.buffer, '123', (70, 80), 90, font, fill=(255,255,255))
-sendValue = 80
-draw_rotated_text(disp.buffer, 'Expense: ' + str(20), (110, 90), 90, font, fill=(255,255,255))
+### refresh image
+while True:
+    onesDigit = gpio.input(pushButton18)
+    tensDigit = gpio.input(pushButton5)
+    send = gpio.input(pushButton3)
+    print "in whike loo"
+    if (onesDigit == False):
+        sendValue += 1
+        print(sendValue)
+        disp.clear((102, 153, 204))
+        # Virtual Credit Card number
+        draw_rotated_text(disp.buffer, '1234-5678-9012-3456', (35, 35), 90, font, fill=(255,255,255))
+        # Expiration Date
+        draw_rotated_text(disp.buffer, '11/05', (70, 180), 90, font, fill=(255,255,255))
+        # Special Code
+        draw_rotated_text(disp.buffer, '567', (70, 80), 90, font, fill=(255,255,255))
+        draw_rotated_text(disp.buffer, 'Expense: ' + str(sendValue), (110, 90), 90, font, fill=(255,255,255))
+        disp.display()
+    if (tensDigit == False):
+        sendValue += 10
+        print(sendValue)
+        disp.clear((102, 153, 204))
+        # Virtual Credit Card number
+        draw_rotated_text(disp.buffer, '1234-1234-1234-1234', (35, 35), 90, font, fill=(255,255,255))
+        # Expiration Date
+        draw_rotated_text(disp.buffer, '01/24', (70, 180), 90, font, fill=(255,255,255))
+        # Special Code
+        draw_rotated_text(disp.buffer, '123', (70, 80), 90, font, fill=(255,255,255))
+        draw_rotated_text(disp.buffer, 'Expense: ' + str(sendValue), (110, 90), 90, font, fill=(255,255,255))
+        disp.display()
+    if (send == False):
+        break
+    time.sleep(0.3)
 
-
-disp.display()
 
 
